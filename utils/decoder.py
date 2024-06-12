2243b8be8c59b89a691f5200e0d6c080d376ac32
Aarav Monga <armonga@usc.edu>
2024-06-13 03:23:57 +0530

    Create decoder.py

---
+++ utils/decoder.py
@@ -0,0 +1,16 @@
+import torch
+import torchaudio
+import numpy as np
+
+class GreedyCTCDecoder(torch.nn.Module):
+    def __init__(self, labels, blank=0):
+        super().__init__()
+        self.labels = labels
+        self.blank = blank
+
+    def forward(self, emission: torch.Tensor) -> List[str]:
+        indices = torch.argmax(emission, dim=-1)  # [num_seq,]
+        indices = torch.unique_consecutive(indices, dim=-1)
+        indices = [i for i in indices if i != self.blank]
+        joined = "".join([self.labels[i] for i in indices])
+        return joined.replace("|", " ").strip().split()
